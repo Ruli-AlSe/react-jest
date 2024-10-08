77787f9f61caaae1997fc45251d629531524e0eb
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-08 02:50:00 -0600

    Create some basic tests to sear for form elements and  validate required inputs on submit

--- user-management-front/src/pages/login/LoginPage.test.tsx
+++ user-management-front/src/pages/login/LoginPage.test.tsx
@@ -1,4 +1,6 @@
 import {screen, render} from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
 import LoginPage from "./LoginPage"
 
 test("It should render the login title", () => {
@@ -6,4 +8,24 @@ test("It should render the login title", () => {
   render(<LoginPage />)
 
   expect(screen.getByRole("heading", {name: /login/i})).toBeInTheDocument()
+})
+
+test("It should render the form elements", () => {
+  render(<LoginPage />)
+
+  expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
+  expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
+  expect(screen.getByRole("button", {name: /submit/i})).toBeInTheDocument()
+})
+
+test("It should validate the inputs as required", async () => {
+  render(<LoginPage />)
+
+  userEvent.click(screen.getByRole("button", {name: /submit/i}))
+
+  // Operation "findByText" returns a promise and is good for this case that "onSubmit" function changes the value of the state
+  expect(await screen.findByText(/The email is required/i)).toBeInTheDocument()
+  expect(
+    await screen.findByText(/The password is required/i),
+  ).toBeInTheDocument()
 })
