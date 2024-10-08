b2e7b7216ec10438576d3de3a22d2a1a7b0b8391
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-08 14:24:28 -0600

    Refactor the login form using react-hook-form and ensuring the integrity of the tests

--- user-management-front/src/pages/login/LoginPage.tsx
+++ user-management-front/src/pages/login/LoginPage.tsx
@@ -1,39 +1,36 @@
 import {Typography, TextField, Button} from "@mui/material"
 import {useState} from "react"
+import {SubmitHandler, useForm} from "react-hook-form"
+
+interface Inputs {
+  email: string
+  password: string
+}
 
 export default function LoginPage() {
-  const [emailErrorMsg, setEmailErrorMsg] = useState<string>("")
-  const [passwordErrorMsg, setPasswordErrorMsg] = useState<string>("")
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<Inputs>()
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-
-    const form = event.currentTarget
-    const formElements = form.elements as typeof form.elements & {
-      email: {value: string}
-      password: {value: string}
-    }
-    const {email, password} = formElements
-
-    if (!email.value) {
-      setEmailErrorMsg("The email is required")
-    }
-    if (!password.value) {
-      setPasswordErrorMsg("The password is required")
-    }
-  }
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data)
 
   return (
     <>
       <Typography component="h1">Login</Typography>
 
-      <form onSubmit={handleSubmit}>
-        <TextField name="email" label="Email" helperText={emailErrorMsg} />
+      <form onSubmit={handleSubmit(onSubmit)}>
         <TextField
-          name="password"
+          label="Email"
+          {...register("email", {required: true})}
+          helperText={errors.email && "The email is required"}
+        />
+        <TextField
           label="Password"
           type="password"
-          helperText={passwordErrorMsg}
+          {...register("password", {required: true})}
+          helperText={errors.password && "The password is required"}
         />
 
         <Button type="submit">Submit</Button>
