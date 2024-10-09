19efff285ea74ca02e1db9c2882fc715b8991fde
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-10-09 01:45:28 -0600

    Implement user story "01-login" for login page

--- user-management-front/src/pages/login/LoginPage.tsx
+++ user-management-front/src/pages/login/LoginPage.tsx
@@ -1,38 +1,61 @@
-import {Typography, TextField, Button} from "@mui/material"
-import {SubmitHandler, useForm} from "react-hook-form"
-import {yupResolver} from "@hookform/resolvers/yup"
-import {loginSchema} from "./loginSchema"
-import {Inputs} from "./interfaces"
+import { useState } from "react"
+import { Typography, TextField, Button } from "@mui/material"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { loginSchema } from "./loginSchema"
+import { Inputs } from "./interfaces"
+import { loginService } from "./services"
+import CircularProgress from "@mui/material/CircularProgress"
 
 export default function LoginPage() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
   const {
     register,
     handleSubmit,
-    formState: {errors},
+    formState: { errors },
   } = useForm<Inputs>({
     resolver: yupResolver(loginSchema),
   })
 
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data)
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    setIsLoading(true)
+    const response = await loginService(email, password)
+    if (!response.ok) {
+      setIsError(true)
+      response.status === 401
+        ? setErrorMessage("The email or password are not correct")
+        : setErrorMessage("Unexpected error, please try again")
+    }
+    setIsLoading(false)
+  }
 
   return (
     <>
+      {isLoading && (
+        <CircularProgress role="progressbar" aria-label="loading" />
+      )}
+
       <Typography component="h1">Login</Typography>
+      {isError && <Typography>{errorMessage}</Typography>}
 
       <form onSubmit={handleSubmit(onSubmit)}>
         <TextField
           label="Email"
-          {...register("email", {required: true})}
+          {...register("email", { required: true })}
           helperText={errors.email?.message}
         />
         <TextField
           label="Password"
           type="password"
-          {...register("password", {required: true})}
+          {...register("password", { required: true })}
           helperText={errors.password?.message}
         />
 
-        <Button type="submit">Submit</Button>
+        <Button disabled={isLoading} type="submit">
+          Submit
+        </Button>
       </form>
     </>
   )
